77472d7c6fe553d8354d274e7081168dad5f223a
ak1ra <git@ak1ra.xyz>
2025-02-06 00:44:22 +0800

    fix utils.get_logger setLevel

--- src/lunar_birthday_ical/utils.py
+++ src/lunar_birthday_ical/utils.py
@@ -2,9 +2,8 @@ import logging
 
 
 def get_logger(name: str, levelname=logging.INFO) -> logging.Logger:
-    logging.basicConfig(
-        format="[%(asctime)s][%(name)s][%(levelname)s] %(message)s", level=levelname
-    )
+    logging.basicConfig(format="[%(asctime)s][%(name)s][%(levelname)s] %(message)s")
     logger = logging.getLogger(name=name)
+    logger.setLevel(levelname)
 
     return logger
