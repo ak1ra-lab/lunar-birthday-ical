c0beffb4c8a0408334c1bba80d459d0907a987f4
ak1ra <git@ak1ra.xyz>
2025-03-26 14:42:32 +0800

    pastebin: replace baseurl with base_url

--- tests/test_pastebin.py
+++ tests/test_pastebin.py
@@ -20,7 +20,7 @@ def mock_file():
 def mock_config():
     return {
         "pastebin": {
-            "baseurl": "http://mockbaseurl.com",
+            "base_url": "http://mockbaseurl.com",
             "admin_url": "http://mockbaseurl.com/mockname:mockpassword",
             "expiration": "7d",
         }
@@ -65,7 +65,9 @@ def test_pastebin_update(mock_put, mock_open, mock_file):
 
 @patch("lunar_birthday_ical.pastebin.pastebin_upload")
 @patch("lunar_birthday_ical.pastebin.pastebin_update")
-def test_pastebin_helper_with_admin_url(mock_update, mock_upload, mock_config, mock_file):
+def test_pastebin_helper_with_admin_url(
+    mock_update, mock_upload, mock_config, mock_file
+):
     mock_response = httpx.Response(200, json={"key": "value"})
     mock_upload.return_value = mock_response
     mock_update.return_value = mock_response
@@ -80,7 +82,9 @@ def test_pastebin_helper_with_admin_url(mock_update, mock_upload, mock_config, m
 
 @patch("lunar_birthday_ical.pastebin.pastebin_upload")
 @patch("lunar_birthday_ical.pastebin.pastebin_update")
-def test_pastebin_helper_without_admin_url(mock_update, mock_upload, mock_config, mock_file):
+def test_pastebin_helper_without_admin_url(
+    mock_update, mock_upload, mock_config, mock_file
+):
     mock_response = httpx.Response(200, json={"key": "value"})
     mock_upload.return_value = mock_response
     mock_update.return_value = mock_response
@@ -88,7 +92,7 @@ def test_pastebin_helper_without_admin_url(mock_update, mock_upload, mock_config
     mock_config["pastebin"]["admin_url"] = None
     pastebin_helper(mock_config, mock_file)
     mock_upload.assert_called_once_with(
-        baseurl="http://mockbaseurl.com", file=mock_file, expiration="7d"
+        base_url="http://mockbaseurl.com", file=mock_file, expiration="7d"
     )
 
 
@@ -115,7 +119,9 @@ def test_pastebin_update_no_expiration(mock_put, mock_open, mock_file):
     mock_response = httpx.Response(200, json={"key": "value"})
     mock_put.return_value = mock_response
 
-    response = pastebin_update("http://mockbaseurl.com/mockname:mockpassword", mock_file)
+    response = pastebin_update(
+        "http://mockbaseurl.com/mockname:mockpassword", mock_file
+    )
 
     mock_put.assert_called_once_with(
         "http://mockbaseurl.com/mockname:mockpassword",
