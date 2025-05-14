fc50c57b304e8a78add5747b5584699a032e4eeb
ak1ra <git@ak1ra.xyz>
2025-05-14 18:05:05 +0800

    refactor!: update multiple configuration fields

--- src/lunar_birthday_ical/config.py
+++ src/lunar_birthday_ical/config.py
@@ -1,13 +1,12 @@
 default_config = {
     "global": {
         "timezone": "Asia/Shanghai",
-        "skip_days": 180,
-        "max_events": 20,
-        "max_days": 30000,
-        "interval": 1000,
-        "max_ages": 80,
-        "solar_birthday": False,
-        "lunar_birthday": True,
+        "holiday_keys": [],
+        "year_start": 2025,
+        "year_end": 2042,
+        "days_max": 30000,
+        "days_interval": 1000,
+        "event_keys": [],
         "event_time": "10:00:00",
         "event_hours": 2,
         "reminders": [1, 3],
@@ -20,22 +19,20 @@ default_config = {
         "admin_url": "",
         "suggest_url": "",
     },
-    "persons": [],
+    "events": [],
 }
 
 tests_config = {
-    "persons": [
+    "events": [
         {
-            "username": "张三",
-            "startdate": "1989-06-03",
-            "solar_birthday": False,
-            "lunar_birthday": True,
+            "name": "张三",
+            "start_date": "1989-06-03",
+            "event_keys": ["lunar_birthday"],
         },
         {
-            "username": "李四",
-            "startdate": "2006-02-01",
-            "solar_birthday": True,
-            "lunar_birthday": False,
+            "name": "李四",
+            "start_date": "2006-02-01",
+            "event_keys": ["integer_days", "solar_birthday"],
         },
     ],
 }
@@ -44,18 +41,16 @@ tests_config_overwride_global = {
     "global": {
         "timezone": "America/Los_Angeles",
     },
-    "persons": [
+    "events": [
         {
-            "username": "张三",
-            "startdate": "1989-06-03",
-            "solar_birthday": False,
-            "lunar_birthday": True,
+            "name": "张三",
+            "start_date": "1989-06-03",
+            "event_keys": ["lunar_birthday"],
         },
         {
-            "username": "李四",
-            "startdate": "2006-02-01",
-            "solar_birthday": True,
-            "lunar_birthday": False,
+            "name": "李四",
+            "start_date": "2006-02-01",
+            "event_keys": ["integer_days", "solar_birthday"],
         },
     ],
 }
